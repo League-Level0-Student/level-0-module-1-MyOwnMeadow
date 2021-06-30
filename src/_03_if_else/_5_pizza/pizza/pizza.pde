4fb7e4eed0c5aee8edff4e48ed6c69fd7ed6e40f
student <student@ip-172-31-51-9.us-west-2.compute.internal>
2021-06-29 18:26:59 -0700

    Pizza Topings and Riddles.

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,12 +1,22 @@
-PImage pepperoni;
-PImage mushroom;
+PImage pep;
+PImage mush;
 PImage olive;
 
 void setup() {
     size(500, 500);
     noStroke();
-}
-void draw() {
+
+//Pizza Topings List
+    pep=loadImage("pepperoni.png");
+    pep.resize(85,85);
+    
+    mush=loadImage("mushroom.png");
+    mush.resize(70,70);
+    
+    olive=loadImage("olive.png");
+    olive.resize(50,50);
+ 
+  //Pizza Base  
     fill(#E5B800);
     ellipse(250, 250, 450, 450);
     
@@ -15,4 +25,18 @@ void draw() {
     
     fill(#FCE000);
     ellipse(250, 250, 380, 390);
+}
+void draw() {
+//Pizza Topings
+  if(mouseButton == LEFT) {
+    image(pep,mouseX-42,mouseY-42);
+  }
+  
+  if(mouseButton == RIGHT) {
+    image(mush,mouseX-45,mouseY-45);
+  }
+  
+  if(keyPressed) {
+    image(olive,mouseX-15,mouseY-15);
+  }
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,25 +1,36 @@
 package _04_int._1_riddler;
-//    Copyright (c) The League of Amazing Programmers 2013-2019
-//    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
-
-		// 1. Make a variable to hold the score
-
-		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
-		// 5. Otherwise, say "wrong" and tell them the answer
-
-		// 6. Add some more riddles
-
+		int score = 0;
+		
+	//Riddle #1
+		String riddle1 = JOptionPane.showInputDialog("What is greater than God, more evil than the devil,\r\n"
+				+ "the poor have it, the rich need it,\r\n"
+				+ "and if you eat it, you'll die? ");
+		
+		if(riddle1.equals("Nothing")) {
+			score += 1;
+			JOptionPane.showMessageDialog(null, "CORRECT!");
+		}
+	
+	//Riddle #2
+		String riddle2 = JOptionPane.showInputDialog("Who makes it has no need of it.\r\n"
+				+ "Who buys it has no use for it.\r\n"
+				+ "Who uses it can neither see nor feel it.\r\n"
+				+ "What is it?");
+		
+		if(riddle2.equals("A coffin")) {
+			score += 1;
+			JOptionPane.showMessageDialog(null, "CORRECT!");
+		}
+		
+	//Riddle #3
+		String riddle3 = JOptionPane.showInputDialog("");
 		// 2. Make a pop up to show the score.
 		
 	}
 }
-
